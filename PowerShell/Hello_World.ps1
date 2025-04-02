3d6379e65ba96cdc8574903a5b8fbe71534a104b
Elvis Gomes <elvisgdeolv@gmail.com>
2025-04-01 22:40:34 -0300

    Update Hello_World.ps1

--- PowerShell/Hello_World.ps1
+++ PowerShell/Hello_World.ps1
@@ -1,2 +1,2 @@
-# Write-Host: Código básico que exibe na tela a mensagem descrita entre parênteses.
+# Write-Host: Código básico que exibe na tela a mensagem descrita entre aspas duplas.
 Write-Host "Olá Mundo!"
